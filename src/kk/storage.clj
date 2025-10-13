4efc91f56eacbdbc9a7a3ee94258ce82a67ef6c9
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-10-13 19:53:12 +0200

    Få med timestamp på dataene

--- src/kk/storage.clj
+++ src/kk/storage.clj
@@ -1,14 +1,25 @@
-(ns kk.storage)
+(ns kk.storage
+  (:import [java.time LocalDateTime]
+           [java.time.format DateTimeFormatter]
+           [java.util Locale]))
 
 (defonce path "history.edn")
 (def latest-bounds 10)
 
+(defn current-datetime []
+  (let [formatter (DateTimeFormatter/ofPattern
+                   "yyyy-MM-dd HH:mm:ss.SSS"
+                   (Locale. "no" "NO"))]
+    (.format (LocalDateTime/now) formatter)))
+
 (defn bounded-conj [limit seq x]
   (take limit (conj seq x)))
 
-(def latest (atom ()))
+(defonce latest (atom ()))
 
 (comment
+
+  (reset! latest ())
 
   @latest
   (first @latest)
@@ -16,5 +27,7 @@
   )
 
 (defn save [data]
-  (reset! latest (bounded-conj latest-bounds @latest data))
-  (spit path (str data "\n") :append true))
+  (let [data (merge {::received (current-datetime)}
+                    data)]
+    (reset! latest (bounded-conj latest-bounds @latest data))
+    (spit path (str data "\n") :append true)))
